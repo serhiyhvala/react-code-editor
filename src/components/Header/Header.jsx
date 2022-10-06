0c0a6e90a425478829e2c0785a2e94284b59e7d3
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-06 13:24:41 +0300

    Add Camera To Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,38 @@
 import React, { useState } from 'react'
 import styles from './Header.module.css'
 import { BsFillCameraVideoFill, BsFillCameraVideoOffFill } from 'react-icons/bs'
+import Webcam from 'react-webcam'
+import Draggable from 'react-draggable'
 
 export const Header = () => {
   const [camera, setCamera] = useState(false)
   return (
-    <header className={styles.header}>
-      <button className={styles.button} onClick={() => setCamera(!camera)}>
-        {camera ? (
-          <BsFillCameraVideoFill fill="#fff" size={23} />
-        ) : (
-          <BsFillCameraVideoOffFill fill="#fff" size={23} />
-        )}
-      </button>
-    </header>
+    <>
+      <header className={styles.header}>
+        <button className={styles.button} onClick={() => setCamera(!camera)}>
+          {camera ? (
+            <BsFillCameraVideoFill fill="#fff" size={23} />
+          ) : (
+            <BsFillCameraVideoOffFill fill="#fff" size={23} />
+          )}
+        </button>
+      </header>
+
+      {camera ? (
+        <Draggable>
+          <Webcam
+            className={styles.camera}
+            audio={false}
+            width={300}
+            height={300}
+            videoConstraints={{
+              width: 300,
+              height: 300,
+              facingMode: 'user'
+            }}
+          />
+        </Draggable>
+      ) : null}
+    </>
   )
 }
