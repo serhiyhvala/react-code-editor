1cc96bbf43e8ec63ac48d05e0f013ec4df9dcecc
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-06 13:23:14 +0300

    Add Preview to App Component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import CodeBar from './components/CodeBar/CodeBar'
 import { EditorProvider } from './context/Context'
 import styles from './App.module.css'
 import { Header } from './components/Header/Header'
-
+import { Preview } from './components/Preview/Preview'
 
 const App = () => {
   return (
     <EditorProvider>
-        <div className={styles.main}>
-          <Header/>
-        </div>
-        <CodeBar />      
+      <div className={styles.main}>
+        <Header />
+      </div>
+      <CodeBar />
+      <Preview />
     </EditorProvider>
   )
 }
